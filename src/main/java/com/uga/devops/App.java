f36fd8b438317e84d7846368424ba9ecc2944247
Augustin Bengolea <Georges.Bengolea@etu.univ-grenoble-alpes.fr>
2019-04-10 10:35:14 +0200

    Merge

--- src/main/java/com/uga/devops/App.java
+++ src/main/java/com/uga/devops/App.java
@@ -8,13 +8,14 @@ public class App {
 //        HashMap<Integer, Column> integerColumnHashMap = new HashMap<Integer, Column>();
         ArrayList<Column> columns = new ArrayList<>();
 
-//        ArrayList<Object> list = new ArrayList<Object>();
-//        list.add(1);
-//        list.add(2);
-//        list.add(3);
-//        list.add(4);
-//        list.add(5);
+        ArrayList<Object> list = new ArrayList<Object>();
+        list.add(1);
+        list.add(2);
+        list.add(null);
+        list.add(4);
+        list.add(5);
 ////        integerColumnHashMap.put(0, new Column("A", list));
+        columns.add(new Column("A", list));
 
 
         ArrayList list2 = new ArrayList();
@@ -28,12 +29,18 @@ public class App {
         columns.add(new Column("B", list2));
 
 
-        for (int lineIndex = 0; lineIndex < columns.size(); lineIndex++) {
-
-        }
+//        for (int lineIndex = 0; lineIndex < columns.size(); lineIndex++) {
+//
+//        }
 
         DataFrame frame = new DataFrame(columns);
         frame.affichage();
+        System.out.println();
+        System.out.println();
+        frame.affichageDebut(3);
+        System.out.println();
+        System.out.println();
+        frame.affichageFin(3);
 
 
 //        for (int i = 0; i < integerColumnHashMap.size(); i++) {
