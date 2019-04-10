49eac9b355a1bb2e49629fd8b706c64590dc89b8
Denis Hodzhadzhikov <hodz.denis@yahoo.com>
2019-04-10 11:14:38 +0200

    Clean App

--- src/main/java/com/uga/devops/App.java
+++ src/main/java/com/uga/devops/App.java
@@ -1,11 +1,9 @@
 package com.uga.devops;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class App {
     public static void main(String[] args) {
-//        HashMap<Integer, Column> integerColumnHashMap = new HashMap<Integer, Column>();
         ArrayList<Column> columns = new ArrayList<>();
 
         ArrayList<Object> list = new ArrayList<Object>();
@@ -14,7 +12,7 @@ public class App {
         list.add(null);
         list.add(4);
         list.add(5);
-////        integerColumnHashMap.put(0, new Column("A", list));
+
         columns.add(new Column("A", list));
 
 
@@ -24,14 +22,9 @@ public class App {
         list2.add("yo 3");
         list2.add("yo 4");
         list2.add("yo 16");
-//        integerColumnHashMap.put(1, );
 
         columns.add(new Column("B", list2));
 
-
-//        for (int lineIndex = 0; lineIndex < columns.size(); lineIndex++) {
-//
-//        }
 
         DataFrame frame = new DataFrame(columns);
         frame.affichage();
@@ -42,9 +35,5 @@ public class App {
         System.out.println();
         frame.affichageFin(3);
 
-
-//        for (int i = 0; i < integerColumnHashMap.size(); i++) {
-//
-//        }
     }
 }
