38d471986baa023a2bba2d38d7aeb00d66e216dc
Denis Hodzhadzhikov <hodz.denis@yahoo.com>
2019-04-14 11:04:04 +0200

    Add csv parser and selection functions

--- src/main/java/com/uga/devops/DataFrame.java
+++ src/main/java/com/uga/devops/DataFrame.java
@@ -2,6 +2,10 @@ package com.uga.devops;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class DataFrame {
 
@@ -11,8 +15,27 @@ public class DataFrame {
         columns = data;
     }
 
-    public DataFrame(File csv) {
-        // TODO
+    public DataFrame(String csv) {
+        try {
+            List<String> allLines = Files.readAllLines(Paths.get(csv));
+            int index = 0;
+            for (String line : allLines) {
+                String[] separated = line.split(";");
+                if (index == 0) {
+                    for (String s : separated) {
+                        Column column = new Column(s, new ArrayList<Object>());
+                        columns.add(column);
+                    }
+                } else {
+                    for (int i = 0; i < separated.length; i++) {
+                        columns.get(i).add(separated[i]);
+                    }
+                }
+                index++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void affichage() {
@@ -98,6 +121,28 @@ public class DataFrame {
             }
             System.out.println(builder.toString());
         }
+    }
+
+    public DataFrame iloc(int pos) {
+        ArrayList<Column> result = new ArrayList<>();
+        for (Column column : columns) {
+            ArrayList<Object> values = new ArrayList<>();
+            values.add(column.getValueAt(pos));
+            result.add(new Column(column.getLabel(), values));
+        }
+
+        return new DataFrame(result);
+    }
+
+    public DataFrame loc(String label) {
+        ArrayList<Column> result = new ArrayList<>();
+        for (Column column : columns) {
+            if (column.getLabel().equals(label)) {
+                result.add(column);
+            }
+        }
+
+        return new DataFrame(result);
     }
 
     private int maxColumnLines() {
